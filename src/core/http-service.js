6d66ce07e8f99d40e6f3e1e448ac7c25cb6f3b3d
vahide <vahidefarzane@gmail.com>
2023-10-14 19:43:54 +0330

    base url changed

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
 // const BASE_URL = "https://react-mini-projects-api.classbon.com";
-const BASE_URL = "https://dashboard-admin-85wg.vercel.app/";
+const BASE_URL = "https://dashboard-admin-85wg.vercel.app";
 
 export const httpService = axios.create({
   baseURL: BASE_URL,
